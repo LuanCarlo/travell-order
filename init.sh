a788eba2f0d1b7a0913838ba12d8bcc97b1d8fec
LuanCarlo <luancarlo.paulo@gmail.com>
2025-10-24 20:22:52 -0300

    alteração .env.example

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ echo "Aguardando banco iniciar..."
 sleep 15
 
 # Gerar .env, key e migrar (ajuste se já existir .env)
-docker cp travell-order-api/.env travell-order-php:/var/www/.env
+docker cp travell-order-api/.env.example travell-order-php:/var/www/.env
 docker exec -it travell-order-php bash -lc "php artisan key:generate"
 docker exec -it travell-order-php bash -lc "php artisan jwt:secret"
 docker exec -it travell-order-php bash -lc "php artisan migrate --force"
